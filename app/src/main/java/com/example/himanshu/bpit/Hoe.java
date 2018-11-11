de416efa72ea750ab9ce300de1a171c55316dd55
himanshunautiyal31198 <himanshu31198@gmail.com>
2018-11-11 19:09:23 +0530

    Login Page setup

--- app/src/main/java/com/example/himanshu/bpit/Hoe.java
+++
@@ -1,4 +0,0 @@
-package com.example.himanshu.bpit;
-
-public class Hoe {
-}
--- app/src/main/java/com/example/himanshu/bpit/InfoActivity.java
+++ app/src/main/java/com/example/himanshu/bpit/InfoActivity.java
@@ -1,13 +1,23 @@
 package com.example.himanshu.bpit;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.example.himanshu.bpit.Fragment.FragmentRollNo;
 
 public class InfoActivity extends AppCompatActivity {
-
+FrameLayout infoFrame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+     infoFrame=findViewById(R.id.infoFrame);
+        FragmentRollNo fragmentRollNo=new FragmentRollNo();
+        FragmentTransaction ftrax=getSupportFragmentManager().beginTransaction();
+        ftrax.replace(R.id.infoFrame,fragmentRollNo);
+        ftrax.commit();
     }
 }
